6195b188925854f43cf1105e3a5d99c05cd025b7
Rangel-Cruz-Macapagal <angelomacapagal52@gmail.com>
2016-10-15 22:21:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ function GuessTheNumberCtrl($scope) {
         $scope.deviation = $scope.original - $scope.guess;
         $scope.noOfTries = $scope.noOfTries + 1;
     }
+    
     $scope.initializeGame = function() {
         $scope.noOfTries = 0;
         $scope.original = Math.floor((Math.random() * 1000) + 1);
         $scope.guess = null;
         $scope.deviation = null;
     }
+    
     $scope.initializeGame();
+   
 }
